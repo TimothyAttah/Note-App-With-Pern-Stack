09e2c699697793cab6a6a148033f3dafc9ccd05a
Timothy Attah <timothyattah733@gmail.com>
2021-06-21 16:02:43 +0100

    Added media queries to modal component

--- client/src/components/modal/index.tsx
+++ client/src/components/modal/index.tsx
@@ -1,10 +1,10 @@
-import React, { FC, Fragment, ReactNode } from 'react';
+import { FC, Fragment, ReactNode } from 'react';
 import ReactDom from 'react-dom';
 import styled from 'styled-components';
 
 const ModalContainer = styled.div`
 	position: absolute;
-	top: 200px;
+	top: 150px;
 	left: 50%;
 	transform: translateX(-50%);
 	max-width: 600px;
@@ -14,6 +14,19 @@ const ModalContainer = styled.div`
 	box-sizing: border-box;
 	padding: 10px 30px;
 	border-radius: 10px;
+	@media (max-width: 600px){
+		max-width: 400px;
+	}
+	@media (max-width: 415px){
+		max-width: 350px;
+	}
+	@media (max-width: 360px){
+		max-width: 300px;
+		top: 120px;
+	}
+	@media (max-width: 290px){
+		max-width: 250px;
+	}
 `;
 
 type ModalProps = {
