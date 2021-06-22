be7b47e2cb982c8e4803f1d96b556663419a2c54
Timothy Attah <timothyattah733@gmail.com>
2021-06-22 17:12:33 +0100

    Created BudgetsFormStyles file

--- client/src/components/forms/BudgetsForm.tsx
+++ client/src/components/forms/BudgetsForm.tsx
@@ -2,96 +2,13 @@ import { FC, FormEvent, Fragment, useState } from 'react'
 import { Button } from '@material-ui/core';
 import { Add,  } from '@material-ui/icons';
 import { useDispatch } from 'react-redux';
-import styled from 'styled-components';
 
 import { createIncomes } from '../../redux/actions/incomesActions';
 import { createExpenses } from '../../redux/actions/expensesActions';
 import { v4 } from 'uuid';
 import { BudgetsModal } from '../modal/BudgetsModal';
+import {ModalBox, ExpensesForm, IncomesForm} from './BudgetsFormStyles'
 
-const ModalBox = styled.div`
-	display: flex;
-	align-items: center;
-	margin-left: 100px;
-  @media (max-width: 780px){
-    margin-left: 10px;
-  }
-  @media (max-width: 700px){
-    margin-left: 70px;
-  }
-  @media (max-width: 550px){
-    margin-left: 55px;
-  }
-  @media (max-width: 415px){
-    margin-left: 38px;
-  }
-  @media (max-width: 320px){
-    margin-left: 30px;
-  }
-`;
-
-const ExpensesForm = styled.form`
-	max-width: 980px;
-	width: 100%;
-	border: 2px dashed blue;
-	margin: 0 auto;
-	margin-bottom: 50px;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	label {
-		color: red;
-		font-weight: 550;
-		margin: 0 10px;
-	}
-	input {
-		padding: 12px;
-		border: 2px solid var(--crimson);
-		border-radius: 5px;
-	}
-	.input-text {
-		width: 400px;
-	}
-	.MuiButton-contained {
-		background-color: var(--crimson);
-		color: var(--white);
-		padding: 10px 0;
-		margin: 0 10px;
-	}
-
-	@media (max-width: 700px) {
-		flex-direction: column;
-		align-items: flex-start;
-		label {
-			margin-left: 70px;
-		}
-		input,
-		.input-text,
-		.MuiButton-contained {
-			width: 80%;
-			margin: 10px auto;
-			padding: 15px;
-		}
-		.input-text {
-			margin-bottom: 20px;
-		}
-	}
-	@media (max-width: 550px) {
-		label {
-			margin-left: 55px;
-		}
-	}
-	@media (max-width: 415px) {
-		label {
-			margin-left: 40px;
-		}
-	}
-	@media (max-width: 320px) {
-		label {
-			margin-left: 30px;
-		}
-	}
-`;
 
 export const BudgetsForm: FC = () => {
   	const [isOpen, setIsOpen] = useState(false);
@@ -108,11 +25,11 @@ export const BudgetsForm: FC = () => {
 			};
 
 			if (isOpen) {
-        if (description || value !== '') {
+        if (description && value !== '') {
           	dispatch(createExpenses(newTransaction));
         }
 			} else {
-        if (description || value !== '') {
+        if (description && value !== '') {
           dispatch(createIncomes(newTransaction));
         }
 			}
@@ -150,7 +67,7 @@ export const BudgetsForm: FC = () => {
 					</Button>
 				</ExpensesForm>
 			) : (
-				<form onSubmit={handleSubmit}>
+				<IncomesForm onSubmit={handleSubmit}>
 					<label>Income Item:</label>
 					<input
 						type='text'
@@ -176,7 +93,7 @@ export const BudgetsForm: FC = () => {
 					>
 						<Add />
 					</Button>
-				</form>
+				</IncomesForm>
 			)}
 		</Fragment>
 	);
---
+++ client/src/components/forms/BudgetsFormStyles.ts
@@ -0,0 +1,153 @@
+import styled from 'styled-components';
+
+export const ModalBox = styled.div`
+	display: flex;
+	align-items: center;
+	margin-left: 100px;
+  margin-top: 20px;
+	@media (max-width: 780px) {
+		margin-left: 10px;
+	}
+	@media (max-width: 700px) {
+		margin-left: 70px;
+	}
+	@media (max-width: 550px) {
+		margin-left: 55px;
+	}
+	@media (max-width: 415px) {
+		margin-left: 38px;
+	}
+	@media (max-width: 320px) {
+		margin-left: 30px;
+	}
+`;
+
+export const ExpensesForm = styled.form`
+	max-width: 980px;
+	width: 100%;
+	margin: 0 auto;
+	margin-bottom: 50px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	label {
+		color: red;
+		font-weight: 550;
+		margin: 0 10px;
+	}
+	input {
+		padding: 12px;
+		border: 2px solid var(--crimson);
+		border-radius: 5px;
+	}
+	.input-text {
+		width: 400px;
+	}
+	.MuiButton-contained {
+		background-color: var(--crimson);
+		color: var(--white);
+		padding: 10px 0;
+		margin: 0 10px;
+		:hover {
+			background-color: var(--crimson);
+		}
+	}
+
+	@media (max-width: 700px) {
+		flex-direction: column;
+		align-items: flex-start;
+		label {
+			margin-left: 70px;
+		}
+		input,
+		.input-text,
+		.MuiButton-contained {
+			width: 80%;
+			margin: 10px auto;
+			padding: 15px;
+		}
+		.input-text {
+			margin-bottom: 20px;
+		}
+	}
+	@media (max-width: 550px) {
+		label {
+			margin-left: 55px;
+		}
+	}
+	@media (max-width: 415px) {
+		label {
+			margin-left: 40px;
+		}
+	}
+	@media (max-width: 320px) {
+		label {
+			margin-left: 30px;
+		}
+	}
+`;
+
+export const IncomesForm = styled.form`
+	max-width: 980px;
+	width: 100%;
+	margin: 0 auto;
+	margin-bottom: 50px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	label {
+		color: green;
+		font-weight: 550;
+		margin: 0 10px;
+	}
+	input {
+		padding: 12px;
+		border: 2px solid green;
+		border-radius: 5px;
+	}
+	.input-text {
+		width: 400px;
+	}
+	.MuiButton-contained {
+		background-color: green;
+		color: var(--white);
+		padding: 10px 0;
+		margin: 0 10px;
+		:hover {
+			background-color: green;
+		}
+	}
+
+	@media (max-width: 700px) {
+		flex-direction: column;
+		align-items: flex-start;
+		label {
+			margin-left: 70px;
+		}
+		input,
+		.input-text,
+		.MuiButton-contained {
+			width: 80%;
+			margin: 10px auto;
+			padding: 15px;
+		}
+		.input-text {
+			margin-bottom: 20px;
+		}
+	}
+	@media (max-width: 550px) {
+		label {
+			margin-left: 55px;
+		}
+	}
+	@media (max-width: 415px) {
+		label {
+			margin-left: 40px;
+		}
+	}
+	@media (max-width: 320px) {
+		label {
+			margin-left: 30px;
+		}
+	}
+`;
