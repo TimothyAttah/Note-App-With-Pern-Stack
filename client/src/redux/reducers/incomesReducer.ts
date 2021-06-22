b03b8c2780681010ecb315e933ed54b236cd3710
Timothy Attah <timothyattah733@gmail.com>
2021-06-22 02:19:26 +0100

    Created incomesReducer.ts

---
+++ client/src/redux/reducers/incomesReducer.ts
@@ -0,0 +1,60 @@
+import { v4 } from 'uuid';
+import { IncomesActionTypes } from '../actionsTypes/incomesTypes';
+import { InitialIncomes } from '../Interface';
+import { IncomesTypes } from '../types';
+
+const initialState: InitialIncomes = {
+	incomes: [
+		{
+			id: v4(),
+			description: 'Finish projects',
+			value: 3000,
+		},
+		{
+			id: v4(),
+			description: 'Salary',
+			value: 4000,
+		},
+		{
+			id: v4(),
+			description: 'Testing',
+			value: 1000,
+		},
+		{
+			id: v4(),
+			description: 'Side job',
+			value: 2000,
+		},
+	],
+};
+
+export const incomesReducer = (
+	state = initialState,
+	action: IncomesActionTypes
+) => {
+	switch (action.type) {
+		case IncomesTypes.LISTS_INCOMES:
+			return {
+				...state,
+			};
+		case IncomesTypes.CREATE_INCOMES:
+			return {
+				...state,
+				incomes: [action.payload, ...state.incomes],
+			};
+		case IncomesTypes.EDIT_INCOMES:
+			return {
+				...state,
+				incomes: state.incomes.map(expense =>
+					expense.id === action.payload.id ? action.payload : expense
+				),
+			};
+		case IncomesTypes.DELETE_INCOMES:
+			return {
+				...state,
+				incomes: state.incomes.filter(income => income.id !== action.payload),
+			};
+		default:
+			return state;
+	}
+};
