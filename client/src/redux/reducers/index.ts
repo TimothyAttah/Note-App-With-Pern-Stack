078d147e8ae5069222519064a6a324e4e072e4e9
Timothy Attah <timothyattah733@gmail.com>
2021-06-22 02:21:05 +0100

    added incomeReducer to reducer

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,17 +1,20 @@
 import { combineReducers } from 'redux';
-import { InitialExpenses, InitialTodos, InitialUser } from '../Interface';
+import { InitialExpenses, InitialIncomes, InitialTodos, InitialUser } from '../Interface';
 import {authReducer} from './authReducer';
 import { expensesReducer } from './expensesReducer';
+import { incomesReducer } from './incomesReducer';
 import {todosReducer} from './todosReducer';
 
 export interface StoreState {
 	todos: InitialTodos;
 	auth: InitialUser;
 	expenses: InitialExpenses;
+	incomes: InitialIncomes;
 }
 
 export const reducers = combineReducers({
 	todos: todosReducer,
 	auth: authReducer,
-	expenses: expensesReducer
+	expenses: expensesReducer,
+	incomes: incomesReducer
 });
