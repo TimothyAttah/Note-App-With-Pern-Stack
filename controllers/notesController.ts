b112c2b3f69d219b2fb9f9e75976b76c071c5357
Timothy Attah <timothyattah733@gmail.com>
2021-07-05 12:45:08 +0100

    Added new listnotes route

--- controllers/notesController.ts
+++ controllers/notesController.ts
@@ -15,5 +15,20 @@ const notesControllers = {
       console.error(err)
       return res.status(500).json({ error: err })
     }
+  },
+  listsIncomes: async (req: any, res: any) => {
+    try {
+      const { description, values } = req.body;
+      if (!description || !values)
+        return res.status(422).json({ error: 'Please enter all fields' });
+      const incomes = await Incomes.query(
+        'INSERT INTO incomes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
+        [req.user.id, description, values]
+      );
+      res.status(200).json({ message: 'New income added', results: incomes.rows[0] })
+    } catch (err) {
+      console.error(err)
+      return res.status(500).json({ error: err })
+    }
   }
 }
