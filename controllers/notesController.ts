859f17cdd2a59f5c39763d31c515f0488bc1c1bd
Timothy Attah <timothyattah733@gmail.com>
2021-07-20 11:56:19 +0100

    Deleted note delete api route

--- controllers/notesController.ts
+++ controllers/notesController.ts
@@ -34,26 +34,5 @@ const notesControllers = {
 			console.error(err);
 			return res.status(500).json({ error: err });
 		}
-	},
-	deleteExpenses: async (req: any, res: any) => {
-		try {
-			const { id } = req.params;
-			const deleteExpense = await Notes.query(
-				'DELETE FROM expenses WHERE expenses_id = $1 AND user_id = $2 RETURNING *',
-				[id, req.user.id]
-			);
-			if (deleteExpense.rows.length === 0) {
-				return res
-					.status(403)
-					.json({ error: 'This expenses is not yours. Authorization denied.' });
-			}
-			res.status(200).json({
-				message: 'expenses deleted successfully',
-				results: deleteExpense.rows[0],
-			});
-		} catch (err) {
-			console.error(err);
-			return res.status(500).json({ error: err });
-		}
-	},
+	}
 };
