7751d035dc703c738a8d14099ea0959112fa7617
Timothy Attah <timothyattah733@gmail.com>
2021-07-22 22:31:15 +0100

    Initial commits

--- controllers/notesController.ts
+++ controllers/notesController.ts
@@ -18,18 +18,18 @@ const notesControllers = {
 			return res.status(500).json({ error: err });
 		}
 	},
-	listsIncomes: async (req: any, res: any) => {
+	listsNotes: async (req: any, res: any) => {
 		try {
 			const { description, values } = req.body;
 			if (!description || !values)
 				return res.status(422).json({ error: 'Please enter all fields' });
-			const incomes = await Notes.query(
-				'INSERT INTO incomes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
+			const notes = await Notes.query(
+				'INSERT INTO notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
 				[req.user.id, description, values]
 			);
 			res
 				.status(200)
-				.json({ message: 'New note added', results: incomes.rows[0] });
+				.json({ message: 'New note added', results: notes.rows[0] });
 		} catch (err) {
 			console.error(err);
 			return res.status(500).json({ error: err });
