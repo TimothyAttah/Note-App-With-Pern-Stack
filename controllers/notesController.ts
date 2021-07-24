32792417649a964c4af6ca751304d42c08111e89
Timothy Attah <timothyattah733@gmail.com>
2021-07-24 22:45:10 +0100

    Added delete note route

--- controllers/notesController.ts
+++ controllers/notesController.ts
@@ -49,7 +49,24 @@ const notesControllers = {
 				.json({ message: 'My note', results: notes.rows[0] });
 		} catch (err) {
 			console.error(err);
-			return res.status(500).json({ error: err });
+			return res.status(500).json({ error: err.message });
+		}
+	},
+	deleteNote: async (req: any, res: any) => {
+		try {
+			const { description, values } = req.body;
+			if (!description || !values)
+				return res.status(422).json({ error: 'Please enter all fields' });
+			const notes = await Notes.query(
+				'INSERT INTO notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
+				[req.user.id, description, values]
+			);
+			res
+				.status(200)
+				.json({ message: 'My note', results: notes.rows[0] });
+		} catch (err) {
+			console.error(err);
+			return res.status(500).json({ error: err.message });
 		}
 	}
 };
