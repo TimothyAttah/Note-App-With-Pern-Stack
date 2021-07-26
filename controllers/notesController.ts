4f1b0d130afc8fe2ae394e22a649b855796aaa9b
Timothy Attah <timothyattah733@gmail.com>
2021-07-26 23:00:37 +0100

    Deleted listNote, deleteNote api router

--- controllers/notesController.ts
+++ controllers/notesController.ts
@@ -7,7 +7,7 @@ const notesControllers = {
 			if (!description || !values)
 				return res.status(422).json({ error: 'Please enter all fields' });
 			const incomes = await Notes.query(
-				'INSERT INTO incomes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
+				'INSERT INTO notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
 				[req.user.id, description, values]
 			);
 			res
@@ -16,57 +16,6 @@ const notesControllers = {
 		} catch (err) {
 			console.error(err);
 			return res.status(500).json({ error: err });
-		}
-	},
-	listsNotes: async (req: any, res: any) => {
-		try {
-			const { description, values } = req.body;
-			if (!description || !values)
-				return res.status(422).json({ error: 'Please enter all fields' });
-			const notes = await Notes.query(
-				'INSERT INTO notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
-				[req.user.id, description, values]
-			);
-			res
-				.status(200)
-				.json({ message: 'New note added', results: notes.rows[0] });
-		} catch (err) {
-			console.error(err);
-			return res.status(500).json({ error: err });
-		}
-	},
-	getNote: async (req: any, res: any) => {
-		try {
-			const { description, values } = req.body;
-			if (!description || !values)
-				return res.status(422).json({ error: 'Please enter all fields' });
-			const notes = await Notes.query(
-				'INSERT INTO notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
-				[req.user.id, description, values]
-			);
-			res
-				.status(200)
-				.json({ message: 'My note', results: notes.rows[0] });
-		} catch (err) {
-			console.error(err);
-			return res.status(500).json({ error: err.message });
-		}
-	},
-	deleteNote: async (req: any, res: any) => {
-		try {
-			const { description, values } = req.body;
-			if (!description || !values)
-				return res.status(422).json({ error: 'Please enter all fields' });
-			const notes = await Notes.query(
-				'DELETE notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
-				[req.user.id, description, values]
-			);
-			res
-				.status(200)
-				.json({ message: 'My note', results: notes.rows[0] });
-		} catch (err) {
-			console.error(err);
-			return res.status(500).json({ error: err.message });
 		}
 	}
 };
