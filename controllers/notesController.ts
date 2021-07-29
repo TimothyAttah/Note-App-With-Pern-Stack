4fc6a62c8ec1fa03def4c9067485d0fd3cfd7bcf
Timothy Attah <timothyattah733@gmail.com>
2021-07-29 18:29:26 +0100

    Added new codes

--- controllers/notesController.ts
+++ controllers/notesController.ts
@@ -17,5 +17,39 @@ const notesControllers = {
 			console.error(err);
 			return res.status(500).json({ error: err });
 		}
+	},
+	createIncome: async (req: any, res: any) => {
+		try {
+			const { description, values } = req.body;
+			if (!description || !values)
+				return res.status(422).json({ error: 'Please enter all fields' });
+			const incomes = await Notes.query(
+				'INSERT INTO notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
+				[req.user.id, description, values]
+			);
+			res
+				.status(200)
+				.json({ message: 'New income added', results: incomes.rows[0] });
+		} catch (err) {
+			console.error(err);
+			return res.status(500).json({ error: err });
+		}
+	},
+	createExpenses: async (req: any, res: any) => {
+		try {
+			const { description, values } = req.body;
+			if (!description || !values)
+				return res.status(422).json({ error: 'Please enter all fields' });
+			const incomes = await Notes.query(
+				'INSERT INTO notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
+				[req.user.id, description, values]
+			);
+			res
+				.status(200)
+				.json({ message: 'New income added', results: incomes.rows[0] });
+		} catch (err) {
+			console.error(err);
+			return res.status(500).json({ error: err });
+		}
 	}
 };
