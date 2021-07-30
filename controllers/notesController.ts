7b5dec0b009431b729497c4147bd7c337644e3c9
Timothy Attah <timothyattah733@gmail.com>
2021-07-30 21:18:10 +0100

    change descrition to notes

--- controllers/notesController.ts
+++ controllers/notesController.ts
@@ -35,18 +35,18 @@ const notesControllers = {
 			return res.status(500).json({ error: err });
 		}
 	},
-	createExpenses: async (req: any, res: any) => {
+	createNotes: async (req: any, res: any) => {
 		try {
-			const { description, values } = req.body;
-			if (!description || !values)
+			const { contents, values } = req.body;
+			if (!contents || !values)
 				return res.status(422).json({ error: 'Please enter all fields' });
 			const incomes = await Notes.query(
-				'INSERT INTO notes(user_id, description, values) VALUES($1, $2, $3) RETURNING * ',
-				[req.user.id, description, values]
+				'INSERT INTO notes(user_id, contents, values) VALUES($1, $2, $3) RETURNING * ',
+				[req.user.id, contents, values]
 			);
 			res
 				.status(200)
-				.json({ message: 'New income added', results: incomes.rows[0] });
+				.json({ message: 'New notes added', results: incomes.rows[0] });
 		} catch (err) {
 			console.error(err);
 			return res.status(500).json({ error: err });
