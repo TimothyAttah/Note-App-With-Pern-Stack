f06abb20dda3806475c55769bb0104bc50f7cabf
Timothy Attah <timothyattah733@gmail.com>
2021-08-08 23:03:06 +0100

    Format files

--- client/src/redux/actionsTypes/todosTypes.ts
+++ client/src/redux/actionsTypes/todosTypes.ts
@@ -2,36 +2,42 @@ import { ITodos } from '../Interface';
 import { TodosTypes } from '../types';
 
 interface ListsTodos {
-  type: TodosTypes.LISTS_TODOS;
-  payload: object;
+	type: TodosTypes.LISTS_TODOS;
+	payload: object;
 }
 
 interface ListsTodo {
-  type: TodosTypes.LISTS_TODO;
-  payload: object;
+	type: TodosTypes.LISTS_TODO;
+	payload: object;
 }
 
 interface CreateTodos {
-  type: TodosTypes.CREATE_TODOS;
-  payload: object;
+	type: TodosTypes.CREATE_TODOS;
+	payload: object;
 }
 
 interface EditTodos {
-  type: TodosTypes.EDIT_TODOS;
-  payload: {
-    todo_id: number;
-    task: string
-  }
+	type: TodosTypes.EDIT_TODOS;
+	payload: {
+		todo_id: number;
+		task: string;
+	};
 }
 
 interface DeleteTodos {
-  type: TodosTypes.DELETE_TODOS;
-  payload: number;
+	type: TodosTypes.DELETE_TODOS;
+	payload: number;
 }
 
 interface ToggleTodos {
-  type: TodosTypes.TOGGLE_TODOS;
-  payload: ITodos;
+	type: TodosTypes.TOGGLE_TODOS;
+	payload: ITodos;
 }
 
-export type TodosActionTypes = ListsTodos | CreateTodos | EditTodos | DeleteTodos | ToggleTodos | ListsTodo
+export type TodosActionTypes =
+	| ListsTodos
+	| CreateTodos
+	| EditTodos
+	| DeleteTodos
+	| ToggleTodos
+	| ListsTodo;
