589234e662a13c66cf56e431d9c1bf9bfa023458
Timothy Attah <timothyattah733@gmail.com>
2021-08-27 20:58:38 +0100

    Setup server and mongoDB

--- Server.ts
+++ Server.ts
@@ -2,20 +2,20 @@ require('dotenv').config({ path: './config/.env' });
 const express = require('express');
 const cors = require('cors');
 
-const app = express()
+const connectingDB = require('./config/db');
+
+const app = express();
+connectingDB();
+
 app.use(express.json());
 app.use(cors());
 
- app.use('/api/v1/auth', require('./routes/authRoutes'));
- app.use('/api/v1/todos', require('./routes/todosRoutes'));
-// app.use('/expenses', require('./routes/expensesRoutes'));
-
 app.use('/', (req: any, res: any) => {
-  hello: 'This is a testing app.'
+  res.send('Hello from Note3Sixty')
 })
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is set on running on port: ${PORT}`);
-});
+  console.log(`Server up and running on port ${PORT}`);
+})
--- config/db.ts
+++ config/db.ts
@@ -1,26 +1,17 @@
-// const Pool = require('pg').Pool;
+const mongoose = require('mongoose');
 
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "timothy",
-//   host: "localhost",
-//   port: "5432",
-//   database: "pernstack"
-// })
-
-// module.exports = pool;
-
-
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  // user: "postgres",
-  // password: "timothy",
-  // host: "localhost",
-  // port: "5432",
-  // database: "note3sixty"
-});
-
-module.exports = {
-  query: (text: any, params: any) => pool.query(text, params)
+const connectDB = async () => {
+ try {
+    const mongoDB = await mongoose.connect(process.env.MONGO_URL, {
+			useNewUrlParser: true,
+			// useCreateIndex: true,
+			// useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
+		console.log(`MongoDB connected on ${mongoDB.connection.host}`);
+ } catch (error) {
+   console.log(error);
+ }
 }
+
+module.exports = connectDB;
