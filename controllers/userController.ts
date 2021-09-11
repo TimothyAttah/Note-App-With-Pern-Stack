0cf1b5128c39234f378609fcea5bcf2326c6319c
Timothy Attah <timothyattah733@gmail.com>
2021-09-11 01:33:31 +0100

    Created follow user routes

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -25,6 +25,26 @@ const userControllers = {
 			res.status(500).json({ error: err.message });
 		}
 	},
+	followUser: async (req: any, res: any) => {
+		
+			if (req.body.userId !== req.params.id) {
+				try {
+					const user = await User.findById(req.params.id)
+					const currentUser = await User.findById(req.body.userId)
+					if (!user.followers.includes(req.body.userId)) {
+						await user.updateOne({$push: {followers: req.body.userId}})
+						await currentUser.updateOne({ $push: { followings: req.body.userId } })
+						res.status(200).json('User has been followed')
+					} else {
+							res.status(403).json({ error: "You already follow this user" });
+					}
+				} catch (err) {
+						res.status(500).json({ error: err.message });
+				}
+			} else {
+				res.status(403).json({error: 'You can\'t follow yourself'})
+			}
+	}
 };
 
 module.exports = userControllers;
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -3,5 +3,6 @@ const userAuth = require('../middleware/auth');
 const userController = require('../controllers/userController');
 
 userRouter.get('/:id/user', userAuth, userController.getUser)
+userRouter.get('/:id/follow', userController.followUser)
 
 module.exports = userRouter;
