998bb7c0a126c5f2716ce92963098211494661dc
Timothy Attah <timothyattah733@gmail.com>
2021-09-19 23:30:29 +0100

    Formated documents

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,13 +1,7 @@
-import { MainHeader } from "./MainHeader";
-import { UserHeader } from "./UserHeader";
-import { user } from "../NameInitials";
-
+import { MainHeader } from './MainHeader';
+import { UserHeader } from './UserHeader';
+import { user } from '../NameInitials';
 
 export const Header = () => {
-
-  return (
-    <div>
-      {user ? <MainHeader/> : <UserHeader />}
-    </div>
-  )
-}
+	return <div>{user ? <MainHeader /> : <UserHeader />}</div>;
+};
