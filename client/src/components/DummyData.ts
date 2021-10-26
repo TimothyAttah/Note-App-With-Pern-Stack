726ef7f80a0bab1bb66ba8b4ee244cfcaa07c5a9
Timothy Attah <timothyattah733@gmail.com>
2021-10-26 09:59:22 +0100

    Changed names in dummy data

--- client/src/components/DummyData.ts
+++ client/src/components/DummyData.ts
@@ -6,22 +6,22 @@ export const Users: UserLists = {
 		{
 			id: 1,
 			profilePicture: images.Alex,
-			username: 'Linus Thomas',
+			username: 'Geraldine Okafor',
 		},
 		{
 			id: 2,
 			profilePicture: images.Mary,
-			username: 'John Caleb',
+			username: 'Jacob Maxwell',
 		},
 		{
 			id: 3,
 			profilePicture: images.JaneDoe,
-			username: 'Andrew Triggers',
+			username: 'Hanna Victoria',
 		},
 		{
 			id: 4,
 			profilePicture: images.ProfileIcon,
-			username: 'Stuart Little',
+			username: 'Olamide Johnson',
 		},
 	],
 };
