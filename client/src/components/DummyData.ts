a74141706794c954759abea0917773426a69f577
Timothy Attah <timothyattah733@gmail.com>
2022-01-23 18:21:48 +0100

    Deleted new post in dummy data file

--- client/src/components/DummyData.ts
+++ client/src/components/DummyData.ts
@@ -60,23 +60,5 @@ export const Posts: PostLists = {
 			likes: 1000,
 			comment: 210
 		},
-		{
-			post_id: 6,
-			desc: 'HAPPY NEW YEAR... Have a nice day',
-			photo: images.PostSix,
-			date: '1 mins ago',
-			userId: 4,
-			likes: 100,
-			comment: 67
-		},
-		{
-			post_id: 7,
-			desc: 'Good morning',
-			photo: images.PostSix,
-			date: '1 mins ago',
-			userId: 78,
-			likes: 160,
-			comment: 60
-		},
 	],
 };
