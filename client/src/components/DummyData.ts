bd494b8dd7934b2eb2f03bf96e6b7872bc66811b
Timothy Attah <timothyattah733@gmail.com>
2022-01-29 22:46:01 +0100

    Changed some names in dummydata

--- client/src/components/DummyData.ts
+++ client/src/components/DummyData.ts
@@ -2,63 +2,63 @@ import { PostLists, UserLists } from '../Interface';
 import { images } from './images';
 
 export const Users: UserLists = {
-	userLists: [
-		{
-			id: 1,
-			profilePicture: images.Alex,
-			username: 'Naomi Mac',
-		},
-		{
-			id: 2,
-			profilePicture: images.Mary,
-			username: 'Diana Thorn',
-		},
-		{
-			id: 3,
-			profilePicture: images.JaneDoe,
-			username: 'Hanna Mitchell',
-		},
-		{
-			id: 4,
-			profilePicture: images.ProfileIcon,
-			username: 'Joe Black',
-		},
-		{
-			id: 5,
-			profilePicture: images.JaneDoe,
-			username: 'Naomi Mac',
-		},
-	],
+  userLists: [
+    {
+      id: 1,
+      profilePicture: images.Alex,
+      username: 'Falz Chike',
+    },
+    {
+      id: 2,
+      profilePicture: images.Mary,
+      username: 'Simi Meka',
+    },
+    {
+      id: 3,
+      profilePicture: images.JaneDoe,
+      username: 'Anthony Johnson',
+    },
+    {
+      id: 4,
+      profilePicture: images.ProfileIcon,
+      username: 'Carol White',
+    },
+    {
+      id: 5,
+      profilePicture: images.JaneDoe,
+      username: 'Pete Sanders',
+    },
+  ],
 };
 
 export const Posts: PostLists = {
-	posts: [
-		{
-			post_id: 88,
-			desc: 'Love for all, Hatred for none!!!',
-			photo: images.PostOne,
-			date: '5 days ago',
-			userId: 1,
-			likes: [],
-			comment: 35,
-		},
-		{
-			post_id: 4,
-			desc: 'Whats sup!!! people Welcome to my world!!!',
-			photo: images.PostThree,
-			date: '16 mins ago',
-			userId: 3,
-			likes: 50,
-			comment: 123,
-		},
-		{
-			post_id: 5,
-			desc: 'HAPPY NEW YEAR... Have a nice day',
-			photo: images.PostFive,
-			date: '1 hour ago',
-			userId: 4,
-			likes: 1000,
-			comment: 210
-		},
-	],
+  posts: [
+    {
+      post_id: 88,
+      desc: 'Love for all, Hatred for none!!!',
+      photo: images.PostOne,
+      date: '5 days ago',
+      userId: 1,
+      likes: [],
+      comment: 35,
+    },
+    {
+      post_id: 4,
+      desc: 'Whats sup!!! people Welcome to my world!!!',
+      photo: images.PostThree,
+      date: '16 mins ago',
+      userId: 3,
+      likes: 50,
+      comment: 123,
+    },
+    {
+      post_id: 5,
+      desc: 'HAPPY NEW YEAR... Have a nice day',
+      photo: images.PostFive,
+      date: '1 hour ago',
+      userId: 4,
+      likes: 1000,
+      comment: 210,
+    },
+  ],
 };
