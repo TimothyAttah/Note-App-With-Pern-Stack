a5a3e44c80527b9babdb4d84d370dc9d54501746
Timothy Attah <timothyattah733@gmail.com>
2022-01-30 23:42:54 +0100

    Deete commented lines in Backdrop.tsx file

--- client/src/components/Backdrop.tsx
+++ client/src/components/Backdrop.tsx
@@ -3,23 +3,20 @@ import { FC } from 'react';
 import styled from 'styled-components';
 
 interface BackdropProps {
-	close?: MouseEventHandler<HTMLDivElement>;
+  close?: MouseEventHandler<HTMLDivElement>;
 }
 
 const BackdropWrapper = styled.div`
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-	/* display: flex; */
-	/* z-index: 500; */
-	position: fixed;
-	/* align-items: center;
-	justify-content: center; */
-	background-color: rgba(0, 0, 0, 0.5);
-	-webkit-tap-highlight-color: transparent;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  /* z-index: 500; */
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-tap-highlight-color: transparent;
 `;
 
 export const Backdrop: FC<BackdropProps> = ({ close }) => {
-	return <BackdropWrapper onClick={close}></BackdropWrapper>;
+  return <BackdropWrapper onClick={close}></BackdropWrapper>;
 };
