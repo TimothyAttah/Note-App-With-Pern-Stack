73197abe94f035bcb9823cdc82a99bc76a438521
Timothy Attah <timothyattah733@gmail.com>
2022-02-11 20:39:38 +0100

    Added new todo

--- client/src/redux/reducers/todos.ts
+++ client/src/redux/reducers/todos.ts
@@ -1,69 +1,77 @@
-import { TodosTypes } from '../types';
-import { InitialTodos, ITodos } from '../InterfaceRedux';
-import { TodosActionTypes } from '../actionsTypes/todosTypes';
-import { v4 } from 'uuid';
+import { TodosTypes } from "../types";
+import { InitialTodos, ITodos } from "../InterfaceRedux";
+import { TodosActionTypes } from "../actionsTypes/todosTypes";
+import { v4 } from "uuid";
 
 const initialState: InitialTodos = {
-	todos: [
-		{
-			todo_id: v4(),
-			task: 'Go to church',
-			isComplete: true,
-			date: new Date(),
-		},
-		{
-			todo_id: v4(),
-			task: 'Finish homework on unbird',
-			isComplete: false,
-			date: new Date(),
-		},
-		{
-			todo_id: v4(),
-			task: 'Take out trashes',
-			isComplete: true,
-			date: new Date(),
-		},
-	],
-	isLoading: false,
+  todos: [
+    {
+      todo_id: v4(),
+      task: "Go to church",
+      isComplete: true,
+      date: new Date(),
+    },
+    {
+      todo_id: v4(),
+      task: "Finish homework on unbird",
+      isComplete: false,
+      date: new Date(),
+    },
+    {
+      todo_id: v4(),
+      task: "Take out trashes",
+      isComplete: true,
+      date: new Date(),
+    },
+    {
+      todo_id: v4(),
+      task: "Take out bin",
+      isComplete: false,
+      date: new Date(),
+    },
+  ],
+  isLoading: false,
 };
 
 export const todos = (state = initialState, action: TodosActionTypes) => {
-	switch (action.type) {
-		case TodosTypes.TODOS_LISTS:
-		case TodosTypes.TODO_LIST:
-			return {
-				...state,
-			};
+  switch (action.type) {
+    case TodosTypes.TODOS_LISTS:
+    case TodosTypes.TODO_LIST:
+      return {
+        ...state,
+      };
 
-		case TodosTypes.TODO_CREATE:
-			return {
-				...state,
-				todos: [action.payload, ...state.todos],
-			};
-		case TodosTypes.TODO_EDIT:
-			return {
-				...state,
-				todos: state.todos.map((todo: { todo_id: string | number; }) =>
-					todo.todo_id === action.payload.todo_id ? action.payload : todo
-				),
-			};
+    case TodosTypes.TODO_CREATE:
+      return {
+        ...state,
+        todos: [action.payload, ...state.todos],
+      };
+    case TodosTypes.TODO_EDIT:
+      return {
+        ...state,
+        todos: state.todos.map((todo: { todo_id: string | number }) =>
+          todo.todo_id === action.payload.todo_id ? action.payload : todo,
+        ),
+      };
 
-		case TodosTypes.TODO_DELETE:
-
-			return {
-				...state,
-				todos: state.todos.filter((todo: { todo_id: string | number; }) => todo.todo_id !== action.payload),
-			};
-		case TodosTypes.TODO_TOGGLE:
-			return {
-				...state,
-				todos: state.todos.map((todo: ITodos) =>
-					todo === action.payload
-						? { ...todo, isComplete: !todo.isComplete }
-						: todo
-				),
-			};
-		default:
-			return state;
-	}
+    case TodosTypes.TODO_DELETE:
+      return {
+        ...state,
+        todos: state.todos.filter(
+          (todo: { todo_id: string | number }) =>
+            todo.todo_id !== action.payload,
+        ),
+      };
+    case TodosTypes.TODO_TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map((todo: ITodos) =>
+          todo === action.payload
+            ? { ...todo, isComplete: !todo.isComplete }
+            : todo,
+        ),
+      };
+    default:
+      return state;
+  }
 };
